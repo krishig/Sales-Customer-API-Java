3dcdd48d3e9541f530d9ad618c3c3bbeea44e717
devendra lokhande <devendra.lokhande06@gmail.com>
2023-08-09 11:43:51 +0530

    changes for cross origin

--- src/main/java/com/KrishiG/controllers/UserController.java
+++ src/main/java/com/KrishiG/controllers/UserController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/users")
-@CrossOrigin
 public class UserController {
 
     @Autowired
