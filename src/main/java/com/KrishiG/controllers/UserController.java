34d2f5e3dde750dcf15c6cc1fe2a49df27f9add2
sanil <sanilguptagpt18@gmail.com>
2023-11-05 17:20:55 +0530

    Some Modification on SwaggerDoc

--- src/main/java/com/KrishiG/controllers/UserController.java
+++ src/main/java/com/KrishiG/controllers/UserController.java
@@ -3,10 +3,6 @@ package com.KrishiG.controllers;
 import com.KrishiG.dtos.request.UserRequestDto;
 import com.KrishiG.responsesApiMessages.ApiResponseMessage;
 import com.KrishiG.services.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,7 +14,6 @@ import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping("/users")
-@Tag(name="UserController",description = "APIs for UserController!!")
 public class UserController {
 
     @Autowired
@@ -26,13 +21,6 @@ public class UserController {
 
     //create
     @PostMapping
-    @Operation(summary = "Create new user ", description = "API for create new user")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",description = "Success !! | Ok"),
-            @ApiResponse(responseCode = "401", description = "Not Authorized !!"),
-            @ApiResponse(responseCode = "201", description = "Created !!")
-
-    })
     public ResponseEntity<UserRequestDto> createUser(@Valid @RequestBody UserRequestDto userRequestDto)
     {
          UserRequestDto user = userService.createUser(userRequestDto);
@@ -41,7 +29,6 @@ public class UserController {
 
     //update
     @PutMapping("/{userId}")
-    @Operation(summary = "Update user ", description = "API for update user")
     public ResponseEntity<UserRequestDto> updateUser(@Valid @RequestBody UserRequestDto userRequestDto,
                                                      @PathVariable("userId") Long userId)
     {
@@ -51,7 +38,6 @@ public class UserController {
 
     //delete
     @DeleteMapping("/{userId}")
-    @Operation(summary = "Delete user ", description = "API for delete user")
     public ResponseEntity<ApiResponseMessage> deleteUser(@PathVariable("userId") Long userId)
     {
         userService.deleteUser(userId);
@@ -64,7 +50,6 @@ public class UserController {
 
     //getAll
     @GetMapping
-    @Operation(summary = "Get all users ", description = "API for get all users")
     public ResponseEntity<List<UserRequestDto>> getAllUsers()
     {
         List<UserRequestDto> users = userService.getAllUsers();
